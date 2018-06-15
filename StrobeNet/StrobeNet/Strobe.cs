17f34d8e72a029e7a4862b72071865a00b7d0cd5
Fasjeit <tom.makarow@gmail.com>
2018-06-15 11:19:28 +0300

    Switching to core libsodium

--- StrobeNet/StrobeNet/Strobe.cs
+++ StrobeNet/StrobeNet/Strobe.cs
@@ -8,7 +8,7 @@ namespace StrobeNet
         /// <summary>
         /// The size of the authentication tag used in AEAD functions
         /// </summary>
-        const int MacLen = 16;
+        private const int MacLen = 16;
 
 
         void Key(byte[] key)
@@ -33,26 +33,17 @@ namespace StrobeNet
         }
 
 
-        private readonly Dictionary<string, Flag> _operationMap = new Dictionary<string, Flag>()
+        private readonly Dictionary<string, Flag> operationMap = new Dictionary<string, Flag>()
         {
             { "AD",       Flag.flagA},
-
             { "KEY",      Flag.flagA | Flag.flagC},
-
             { "PRF",      Flag.flagI | Flag.flagA | Flag.flagC},
-
             { "send_CLR", Flag.flagA | Flag.flagT},
-
             { "recv_CLR", Flag.flagI | Flag.flagA | Flag.flagT},
-
             { "send_ENC", Flag.flagA | Flag.flagC | Flag.flagT},
-
             { "recv_ENC", Flag.flagI | Flag.flagA | Flag.flagC | Flag.flagT},
-
             { "send_MAC", Flag.flagC | Flag.flagT},
-
             { "recv_MAC", Flag.flagI | Flag.flagC | Flag.flagT},
-
             { "RATCHET", Flag.flagC},
         };
 
@@ -67,7 +58,7 @@ namespace StrobeNet
         byte[] Operate(bool meta, string operation, byte[] dataConst, int length, bool more)
         {
             // operation is valid?
-            if (!_operationMap.TryGetValue(operation, out var flags))
+            if (!this.operationMap.TryGetValue(operation, out var flags))
             {
                 throw new Exception($"Not a valid operation: [{operation}]");
             }
@@ -111,11 +102,37 @@ namespace StrobeNet
                     throw new Exception("Flag should be the same when streaming operations");
                 }
             }
-            else{
+            else {
                 BeginOp(flags);
                 _curFlags = flags;
             }
 
             // Operation
         }
+
+        // beginOp: starts an operation
+        Flag flags BeginOp()
+        {
+
+            if ((flags & Flag.flagT) != 0) {
+                if _I0 == iNone {
+                    _.I0 = role(flags & flagI)
+
+
+        }
+                flags ^= flag(s.I0)
+
+
     }
+
+            oldBegin:= s.posBegin
+
+
+    s.posBegin = uint8(len(s.buf) + 1) // s.pos + 1
+
+    forceF:= (flags & (flagC | flagK) != 0)
+
+
+    s.duplex([]byte{ oldBegin, byte(flags)}, false, false, forceF)
+}
+}
