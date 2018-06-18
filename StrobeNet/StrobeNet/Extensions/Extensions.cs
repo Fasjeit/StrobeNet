6bec7b91e43fc57378e90f1b58792a2caef8f9f2
Fasjeit <tom.makarow@gmail.com>
2018-06-18 23:12:44 +0300

    More comments

--- StrobeNet/StrobeNet/Extensions/Extensions.cs
+++ StrobeNet/StrobeNet/Extensions/Extensions.cs
@@ -5,12 +5,21 @@
 
     public static class Extensions
     {
+
+        /// <summary>
+        /// Converts hex string to byte array
+        /// </summary>
+        /// <param name="hex">String to decode</param>
         public static byte[] ToByteArray(this string hex)
         {
             return Enumerable.Range(0, hex.Length).Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16)).ToArray();
         }
 
+        /// <summary>
+        /// Convers byte array to hex string
+        /// </summary>
+        /// <param name="array">Array to endoce</param>
         public static string ToHexString(this byte[] array)
         {
             return BitConverter.ToString(array).Replace("-", "");
