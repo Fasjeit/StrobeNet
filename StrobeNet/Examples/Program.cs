cec24c4f9fc627ac13cfceda3e7b1e78e377d6f5
Fasjeit <tom.makarow@gmail.com>
2018-06-18 15:58:59 +0300

    Forget to add files of example project

---
+++ StrobeNet/Examples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Samples
+{
+    using System;
+    using System.Text;
+
+    using StrobeNet;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Create strobe object, setting init string and security
+            var strobe = new Strobe("MyStrobe", 128);
+
+            // Authenticating message
+            var messageByte = Encoding.ASCII.GetBytes("Hello gentlemens");
+            strobe.Ad(false, messageByte);
+
+            // Get Prf output
+            var prfBytes = strobe.Prf(16);
+            Console.WriteLine(BitConverter.ToString(prfBytes).Replace("-", ""));
+        }
+    }
+}
