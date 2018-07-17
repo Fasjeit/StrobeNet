94ef48fab8a55c618a301f2bc7d395309ea3bf68
Fasjeit <tom.makarow@gmail.com>
2018-07-17 14:45:23 +0300

    missing enum folder

---
+++ StrobeNet/StrobeNet/Enums/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrobeNet.Enums
+{
+    public enum Operation
+    {
+        Ad,
+        Key,
+        Prf,
+        SendClr,
+        RecvClr,
+        SendEnc,
+        RecvEnc,
+        SendMac,
+        RecvMac,
+        Ratchet,
+    }
+}
